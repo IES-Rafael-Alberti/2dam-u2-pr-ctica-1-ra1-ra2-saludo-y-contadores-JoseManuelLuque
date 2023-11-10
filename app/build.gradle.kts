5e282c4e71aaa2cc561b91464b6646c009fbef7a
José Manuel Luque <jluqgon214@g.educaand.es>
2023-11-10 20:16:04 +0100

    Clase 10/11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.jluqgon214.saludoycontenedores"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.jluqgon214.saludoycontenedores"
--- app/src/main/java/com/jluqgon214/saludoycontenedores/MainActivity.kt
+++ app/src/main/java/com/jluqgon214/saludoycontenedores/MainActivity.kt
@@ -3,13 +3,24 @@ package com.jluqgon214.saludoycontenedores
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.window.Dialog
 import com.jluqgon214.saludoycontenedores.ui.theme.SaludoYContenedoresTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +33,50 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Saludar()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    SaludoYContenedoresTheme {
-        Greeting("Android")
+fun Saludar() {
+    var show by rememberSaveable { mutableStateOf(false) }
+
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        if (show) {
+            ShowDialog()
+        }
+
+        Button(onClick = { show = true }) {
+            Text(text = "Saludar")
+        }
+        Text(text = "")
+    }
+}
+
+
+@Composable
+fun ShowDialog() {
+    var name by rememberSaveable { mutableStateOf("") }
+
+    Row(
+        Modifier.fillMaxSize(),
+        verticalAlignment = Alignment.Top,
+        horizontalArrangement = Arrangement.End
+    ) {
+        Dialog(
+            onDismissRequest = { /*TODO*/ }
+        ) {
+            TextField(value = name, onValueChange = {newName:String -> name = newName}, label = ("Hola")) {
+
+            }
+        }
     }
 }
